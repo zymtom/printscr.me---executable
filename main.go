a6bdaf80bdc517c6ec4da708d983712e16ae427c
zymtom <lolipopzere@gmail.com>
2016-02-11 17:51:19 +0100

    Commented and finished first build
    I commented out all the broken code for the gui, as I can't get that
    working, period. Made a simple bypass for now so it only opens the link
    in the browser. Not the most elegant solution, but it works for now.
    Continuing developing of the gui on another branch another time.

--- main.go
+++ main.go
@@ -9,30 +9,49 @@ import (
     "io"
     "mime/multipart"
     "net/http"
-    "net/url"
+    //"net/url"
     //"io/ioutil"
     "encoding/json"
     "errors"
-    "github.com/getlantern/systray"
-    "github.com/getlantern/systray/example/icon"
+    "os/exec"
+    "runtime"
+    //"github.com/getlantern/systray"
+    //"github.com/getlantern/systray/example/icon"
 )
 
 func main() {
-    systray.Run(onReady)
+    //systray.Run(onReady)
     f, e := makeScreenshot()
     if e != nil {
         panic(e)
     }
-    res, err := Upload("http://127.0.0.1:6969/projects/printscreen.me---website/api.php", "./"+f)
+    res, err := Upload("http://printscr.me/api.php", "./"+f)
     if err != nil {
         panic(err)
     }
     if res["upload"] == "failed" {
         panic(res["reason"])
     }
-    fmt.Print(res["location"])
+    if str, ok := res["location"].(string); !ok {
+        panic("something went wrong with getting the location")
+    }else{
+        var err error
+        switch runtime.GOOS {
+        case "linux":
+            err = exec.Command("xdg-open", str).Start()
+        case "windows", "darwin":
+            err = exec.Command("cmd", "/c", "start", str).Start()
+        default:
+            err = fmt.Errorf("unsupported platform")
+        }
+        if err != nil {
+            panic(err)
+        }
+    }
+    
+    
 }
-func onReady() {
+/*func onReady() {
     systray.SetIcon(icon.Data)
     systray.SetTitle("Awesome App")
     systray.SetTooltip("Pretty awesome")
@@ -42,7 +61,7 @@ func onReady() {
 		systray.Quit()
 		fmt.Println("Quit now...")
 	}()
-}
+}*/
 func makeScreenshot()(file string, err error){
     file = "ss123.png"
     img, err := screenshot.CaptureScreen()
@@ -61,10 +80,8 @@ func makeScreenshot()(file string, err error){
     return file, nil
 }
 func Upload(reqUrl, file string) (response map[string]interface{}, err error) {
-    // Prepare a form that you will submit to that URL.
     var b bytes.Buffer
     w := multipart.NewWriter(&b)
-    // Add your image file
     f, err := os.Open(file)
     if err != nil {
         return nil, err
@@ -76,27 +93,21 @@ func Upload(reqUrl, file string) (response map[string]interface{}, err error) {
     if _, err = io.Copy(fw, f); err != nil {
         return nil, err
     }
-    // Add the other fields
     if fw, err = w.CreateFormField("key"); err != nil {
         return nil, err
     }
     if _, err = fw.Write([]byte("KEY")); err != nil {
         return nil, err
     }
-    // Don't forget to close the multipart writer.
-    // If you don't close it, your request will be missing the terminating boundary.
     w.Close()
-
-    // Now that you have a form, you can submit it to your handler.
     req, err := http.NewRequest("POST", reqUrl, &b)
     if err != nil {
         return nil, err
     }
-    // Don't forget to set the content type, this will contain the boundary.
     req.Header.Set("Content-Type", w.FormDataContentType())
-
-    proxyUrl, err := url.Parse("http://127.0.0.1:8888")
-    client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+    //proxyUrl, err := url.Parse("http://127.0.0.1:8888")
+    //client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+    client := &http.Client{}
     res, err := client.Do(req)
     if err != nil {
         return nil, err
@@ -113,12 +124,10 @@ func Upload(reqUrl, file string) (response map[string]interface{}, err error) {
     if dec == nil {
         return nil, errors.New("error decoding json")
     }
-
     json_map := make(map[string]interface{})
     err = dec.Decode(&json_map)
     if err != nil {
         return nil, errors.New("error decoding json")
     }
-
     return json_map, nil
 }
